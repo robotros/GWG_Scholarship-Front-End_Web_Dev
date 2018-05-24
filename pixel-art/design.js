6b4ee2e9559f3728ab41b95e2a2c32076851e3c6
Aron Roberts <robotros1@msn.com>
2018-05-24 12:14:48 -0700

    doc:updated comments

--- pixel-art/design.js
+++ pixel-art/design.js
@@ -3,36 +3,56 @@ $(document).ready(function() {
   const pixelCanvas = $("#pixelCanvas");
   const sizePicker = $("#sizePicker");
   const colorPicker = $("#colorPicker");
-  const height = $("#inputHeight");
-  const width = $("#inputWidth");
+  const heightInput = $("#inputHeight");
+  const widthInput = $("#inputWidth");
   const dummy = $("#dummy");
-  let color = colorPicker.val();
 
-  // Create h by w grid
-  function makeGrid() {
-    for (let h = 0; h < height.val(); h++) {
+  //other variables
+  let color = colorPicker.val();
+  let height = heightInput.val();
+  let width = widthInput.val();
+
+
+ /**
+* @description Creates a table
+* @param {int} height - Height of the table
+* @param {int} width  - width of the table
+*/
+  function makeGrid(height, width) {
+    for (let h = 0; h < height; h++) {
       pixelCanvas.append("<tr></tr>");
-      for (let w = 0; w < width.val(); w++) {
+      for (let w = 0; w < width; w++) {
         $("tr:last").append("<td></td>");
       }
     }
   }
 
-  // When size is submitted by the user, call makeGrid()
+  /**
+  * @description When size is submitted by the user, call makeGrid()
+  */
   sizePicker.submit(function(event) {
+    // prevent default
     event.preventDefault();
+    // reset canvas
     pixelCanvas.children().remove();
-    makeGrid();
+    // update height and width
+    height = heightInput.val();
+    width = widthInput.val();
+    // call makeGrid()
+    makeGrid(height, width);
   });
 
-  // set color
+   /**
+  * @description sets selected color
+  */
   colorPicker.change(function(event) {
    color=colorPicker.val();
    $(".color").css("color",color);
   });
 
-
-  // change color
+   /**
+  * @description updated color of selected square
+  */
   pixelCanvas.on("click", "td", function() {
     dummy.css("color", color);
     if ($(this).css("background-color") == dummy.css("color")) {
